796987c13463744bb311f181b39f8006ff070428
josspp <josephjputhumana@gmail.com>
2024-11-13 07:46:32 +0530

    new

--- SQL_Learning/sqlDemo1.py
+++ SQL_Learning/sqlDemo1.py
@@ -1,6 +1,18 @@
 import sqlite3
 
-connection = sqlite3.connect(r"D:\Training\Chinook_Sqlite.sqlite")
+connection = sqlite3.connect(r"D:\Training\280624_JosephJPuthumana_Python\SQL_Learning\Chinook_Sqlite.sqlite")
 curr=connection.cursor()
 
 print(curr)
+
+curr.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
+tables=curr.fetchall()
+for table in tables:
+    print(table)
+
+print("Finished")
+
+
+
+
